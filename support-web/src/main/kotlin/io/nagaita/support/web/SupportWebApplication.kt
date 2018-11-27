43ab552c55cd1620215cece797e7be6abd562d44
nagaita <28795978+nagaita@users.noreply.github.com>
2018-11-27 19:33:59 +0900

    feat: #1 Prepare simple web app.

---
+++ support-web/src/main/kotlin/io/nagaita/support/web/SupportWebApplication.kt
@@ -0,0 +1,19 @@
+package io.nagaita.support.web
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SupportWebApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(SupportWebApplication::class.java, *args)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<SupportWebApplication>(*args)
+}
---
+++ support-web/src/test/kotlin/io/nagaita/support/web/SupportWebApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.nagaita.support.web
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SupportWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
